cd212433d01c8e6ee6c14bafdc283380576ffb3b
RadostinKirov <74134419+RadostinKirov@users.noreply.github.com>
2022-03-10 22:35:56 +0200

    My Movie Collection - route fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
 import SearchBar from "../SharedComponents/SearchBar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
     return (
         <header>
             <nav>
-                <div className="my-collection">My Movie Collection</div>
+                <Link to="/">
+                    <div className="my-collection">
+                        My Movie Collection
+                    </div>
+                </Link>
                 <SearchBar />
             </nav>
         </header>
