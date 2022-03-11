bc1cf035b786fb80bdf1287c3cd98aa164a8b46d
RadostinKirov <74134419+RadostinKirov@users.noreply.github.com>
2022-03-11 16:23:02 +0200

    get info from search input

--- src/components/SharedComponents/SearchBar.js
+++ src/components/SharedComponents/SearchBar.js
@@ -1,10 +1,17 @@
 
 const SearchBar = () => {
+ 
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        let data = new FormData(e.currentTarget);
+        let searchedInput = data.get('searchInput');
+        console.log(searchedInput);
+    }
 
     return (
-        <form className="search-bar">
-            <input type="text" placeholder="Search by movie title..." />
-            <input type="button" className="search-btn" value="Search"/>
+        <form className="search-bar" onSubmit={onSubmitHandler}>
+            <input type="text" name="searchInput" placeholder="movie name...." />
+            <input type="submit" className="search-btn" value="Search" />
         </form>
     )
 
