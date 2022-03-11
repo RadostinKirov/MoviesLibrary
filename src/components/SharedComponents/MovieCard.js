02da289fb03de9dc1cbce1fbd3a39cabdc39255d
RadostinKirov <74134419+RadostinKirov@users.noreply.github.com>
2022-03-12 00:09:31 +0200

    url to the movie added in MovieCard

--- src/components/SharedComponents/MovieCard.js
+++ src/components/SharedComponents/MovieCard.js
@@ -1,26 +1,25 @@
 
-import { Link, } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const MovieCard = ({ movieInfo }) => {
 
-console.log(movieInfo)
+    console.log(movieInfo)
     return (
         <section className="movie">
 
             <Link to={`/details/${movieInfo.id}`} className="img-container">
-                <img src={movieInfo.image? movieInfo.image.original : ""} alt={movieInfo.name} />
+                <img src={movieInfo.image ? movieInfo.image.original : ""} alt={movieInfo.name} />
             </Link>
             <div className="movie-info">
-                <Link to="#" className="movie-title">{movieInfo.name} {movieInfo.premiered? `(${movieInfo.premiered.slice(0,4)})` : ""}</Link>
+                <Link to="#" className="movie-title">{movieInfo.name} {movieInfo.premiered ? `(${movieInfo.premiered.slice(0, 4)})` : ""}</Link>
                 <div>
                     <p className="genre">{movieInfo.genres.join(', ')}</p>
-                    <p className="duration">{movieInfo.runtime? `${movieInfo.runtime} minutes` : ''}</p>
+                    <p className="duration">{movieInfo.runtime ? `${movieInfo.runtime} minutes` : ''}</p>
                 </div>
-                <p className="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et recusandae
-                    beatae esse cupiditate, quae aspernatur aliquam provident similique quas. Vitae quos nihil
-                    pariatur magnam perspiciatis numquam doloribus voluptate, error saepe?</p>
-                <Link to="#" className="official-site">Visit official site</Link>
+                {movieInfo.summary}
+                <a href={movieInfo.url}>Visit official site</a>
+               
                 <Link to="" className="add-to-favorites"> Add to Favorites</Link>
 
 
