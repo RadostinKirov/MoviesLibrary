1580788e5e8e16cae431c82fb2f127c42f320335
RadostinKirov <74134419+RadostinKirov@users.noreply.github.com>
2022-03-12 12:59:19 +0200

    Link from poster to Details

--- src/components/Home/MoviePoster.js
+++ src/components/Home/MoviePoster.js
@@ -1,11 +1,25 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import MovieContext from "../../context/movieContext";
+
 
 const MoviePoster = ({ movieInfo }) => {
-console.log('inMoviePoster');
-console.log("MovieInfo in Poster -> ", movieInfo);
+    const { addNewClickedMovie } = useContext(MovieContext);
+    const navigate = useNavigate();
+
+    console.log('inMoviePoster');
+    console.log("MovieInfo in Poster -> ", movieInfo);
+
+
+    const onDetailsClickHandler = (e) => {
+        e.preventDefault();
+        addNewClickedMovie(movieInfo);
+        navigate(`/details/${movieInfo.id}`)
+    }
+
     return (
         <section className="movie-image">
-            <Link to={`/details/${movieInfo.id}`}>
+            <Link to={`/details/${movieInfo.id}`} onClick={onDetailsClickHandler} >
                 <img src={movieInfo.image ? movieInfo.image.original : ""} alt={movieInfo.name} />
             </Link>
         </section>
