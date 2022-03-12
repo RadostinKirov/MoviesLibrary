736913d366561780974d8b68fc58321faec29c74
RadostinKirov <74134419+RadostinKirov@users.noreply.github.com>
2022-03-12 12:06:55 +0200

    title link to details

--- src/components/SharedComponents/MovieCard.js
+++ src/components/SharedComponents/MovieCard.js
@@ -23,7 +23,7 @@ const MovieCard = ({ movieInfo }) => {
                 <img src={movieInfo.image ? movieInfo.image.original : ""} alt={movieInfo.name} />
             </Link>
             <div className="movie-info">
-                <Link to="#" className="movie-title">{movieInfo.name} {movieInfo.premiered ? `(${movieInfo.premiered.slice(0, 4)})` : ""}</Link>
+                <Link to="" onClick={onDetailsClickHandler} className="movie-title">{movieInfo.name} {movieInfo.premiered ? `(${movieInfo.premiered.slice(0, 4)})` : ""}</Link>
                 <div>
                     <p className="genre">{movieInfo.genres.join(', ')}</p>
                     <p className="duration">{movieInfo.runtime ? `${movieInfo.runtime} minutes` : ''}</p>
